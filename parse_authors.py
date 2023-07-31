f6e4860614cd4e120140ec6cb4d75ce24840782f
Phil McMinn <phil@dyn102173.shef.ac.uk>
2023-07-31 16:28:21 +0100

    Parsed authors and venues

--- parse_authors.py
+++ parse_authors.py
@@ -1,36 +1,79 @@
 import bibtexparser
 import bibtexparser.middlewares as m
 import re
-repeated_whitespace_pattern = re.compile(r'\s+')
 
-def author_var_name(author):
-  return author.split(',')[0].lower().replace('-', '').replace(' ', '').replace('{', '').replace('}', '').replace('\\\'', '')
+def author_var_name(raw_author_string): 
+  names = raw_author_string.split(',')
+  surname = names[0].strip()
+  first_names = names[1].strip()
 
-def author_to_string(author):
-  return author.replace('\\' , '\\\\')
+  return (surname
+            .lower()
+            .replace('-', '')
+            .replace(' ', '')
+            .replace('{', '')
+            .replace('}', '')
+            .replace('\\\'', '') 
+            + "_" + 
+            first_names[0].lower())
 
-# We want to add three new middleware layers to our parse stack:
-layers = [
-    m.SeparateCoAuthors(True), # Co-authors should be separated as list of strings
-]
+def author_to_array(raw_author_string):
+  author = (raw_author_string
+            .replace("\\'{e}" , 'é')
+            .replace("{\\'e}", 'é')
+            .replace("\\'{a}", 'á'))
+  
+  names = author.split(',')
+  surname = names[0].strip()
+  first_names = names[1].split()
 
-file = "mcminn.bib"
-library = bibtexparser.parse_file("mcminn.bib", append_middleware=layers)
+  processed_first_names = []
+  for first_name in first_names:
+    split = first_name.split('.')
+    for element in split:
+      if element.strip() != '':
+        processed_first_names.append(element)
 
-authors = set()
+  return [surname] + processed_first_names
 
-for entry in library.entries:
-  entry_authors = entry.fields_dict['author'].value
-  for author in entry_authors:
-    author = re.sub(repeated_whitespace_pattern, ' ', author)
-    authors.add(author)
-      
-authors = sorted(authors)
+def write_authors_file():
+  repeated_whitespace_pattern = re.compile(r'\s+')
 
-f = open('parsed_authors.py', 'w')
+  layers = [
+      m.SeparateCoAuthors(True), # Co-authors should be separated as list of strings
+  ]
 
-for author in authors:
-  define_author = author_var_name(author) + ' = "' + author_to_string(author) + '"'
-  print(define_author, file=f)
+  file = "mcminn.bib"
+  library = bibtexparser.parse_file("mcminn.bib", append_middleware=layers)
 
-f.close()
+  authors = set()
+
+  for entry in library.entries:
+    entry_authors = entry.fields_dict['author'].value
+    for author in entry_authors:
+      author = re.sub(repeated_whitespace_pattern, ' ', author)
+      authors.add(author)
+        
+  authors = sorted(authors)
+
+  f = open('parsed_authors.py', 'w')
+
+  for author in authors:
+    code = author_var_name(author) + ' = ' # the assignment part
+
+    names = author_to_array(author)
+    first = True
+    code = code + '['
+    for name in names:
+      if first:
+        first = False
+      else: 
+        code = code + ', '
+      code = code + '"' + name + '"'    
+    code = code + ']'
+    
+    print(code, file=f)
+
+  f.close()
+
+write_authors_file()
