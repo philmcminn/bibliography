1a2fac974013798802fd1eaa91c4afb5b55f2053
Phil McMinn <phil@penguin-2.broadband>
2023-08-01 15:32:01 +0100

    Black formatting

--- bib.py
+++ bib.py
@@ -1,14 +1,20 @@
-from authors import *
-from venues import *
+def add_pub(bib, key, pub):
+    if key in bib:
+        print('"' + key + '" already in bib')
+        quit()
+    bib[key] = pub
 
-bib = dict()
 
-bib['Parry2023'] = {
-  'author': [parry_o, hilton_m, kapfhammer_g, mcminn_p],
-  'title': 'Empirically evaluating flaky test detection techniques combining test case rerunning and machine learning models',
-  'venue': emse,
-  'volume': '28',
-  'issue': '3',
-  'year': '2023'
-}
+def get_years(bib):
+    years = {inner_dict["year"] for inner_dict in bib.values() if "year" in inner_dict}
+    return sorted(years, reverse=True)
 
+
+def get_pubs(bib, year):
+    pubs = {
+        key: inner_dict
+        for key, inner_dict in bib.items()
+        if "year" in inner_dict and inner_dict["year"] == year
+    }
+    return pubs
+    # return sorted(pub.values(), key=lambda x: x['title'])
---
+++ bib2txt.py
@@ -0,0 +1,68 @@
+from authors import *
+from venues import *
+from bib import *
+from mcminn import *
+
+
+def author_str(author):
+    formatted_name_parts = [name.strip().capitalize() for name in author[1:]]
+    initials = "".join([name[0] for name in formatted_name_parts])
+    return f"{initials} {author[0]}"
+
+
+def authors_str(authors):
+    authors_str = [author_str(author) for author in authors]
+    if len(authors_str) == 0:
+        return ""
+    elif len(authors_str) == 1:
+        return authors_str[0]
+    else:
+        return ", ".join(authors_str[:-1]) + " and " + authors_str[-1]
+
+
+def venue_str(venue):
+    if venue["type"] == Venue.BOOK_CHAPTER:
+        return "In " + venue["name"]
+    elif venue["type"] == Venue.CONFERENCE or venue["type"] == Venue.WORKSHOP:
+        suffix = ""
+        if venue["acronym"] != "":
+            suffix = " (" + venue["acronym"] + ")"
+        return venue["name"] + suffix
+    elif venue["type"] == Venue.JOURNAL:
+        return venue["name"]
+    elif venue["type"] == Venue.PHD_THESIS:
+        return "PhD Thesis, " + venue["name"]
+    elif venue["type"] == Venue.TECH_REPORT:
+        return "Technical Report, " + venue["name"]
+
+
+def pub_str(key, pub):
+    return (
+        authors_str(pub["author"])
+        + ". "
+        + pub["title"]
+        + ". "
+        + venue_str(pub["venue"])
+        + ", "
+        + str(pub["year"])
+        + "."
+    )
+
+
+def format_pubs():
+    count = 0
+    years = get_years(bib)
+
+    for year in years:
+        print("\n" + str(year))
+
+        pubs = get_pubs(bib, year)
+
+        for key, pub in pubs.items():
+            count += 1
+            print("* " + pub_str(key, pub))
+
+    print("\n" + str(count) + " publications.")
+
+
+format_pubs()
