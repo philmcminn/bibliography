e110fbf1205912968fad4708ad548c956587b82c
Phil McMinn <phil.mcminn@gmail.com>
2024-10-18 15:50:18 +0100

    Adding titles to tags

--- tags.py
+++ tags.py
@@ -1,19 +1,64 @@
 agent_based_modelling = {
-    "tag" : "agent-based-modelling",
+    "tag": "agent-based-modelling",
     "title": "Agent-Based Modelling",
 }
-autograding = "autograding"
-database_testing = "database-testing"
-flaky_tests = "flaky-tests"
-mutation_analysis = "mutation-analysis"
-oracles = "oracles"
-pseudo_tested = "pseudo-tested"
-regression_testing = "regression-testing"
-search_based_software_engineering = "search-based-software-engineering"
-search_based_test_generation = "search-based-test-generation"
-software_maintenance = "software-maintenance"
-testability = "testability"
-testability_transformation = "testability-transformation"
-test_case_diversity = "test-case-diversity"
-test_suite_maintenance = "test-suite-maintenance"
-web_presentation_failures = "web-presentation-failures"
+autograding = {
+    "tag": "autograding",
+    "title": "Autograding Student Assignments",
+}
+database_testing = {
+    "tag": "database-testing",
+    "title": "Database Testing",
+}
+flaky_tests = {
+    "tag": "flaky-tests",
+    "title": "Flaky Tests",
+}
+mutation_analysis = {
+    "tag": "mutation-analysis",
+    "title": "Mutation Analysis",
+}
+oracles = {
+    "tag": "oracles",
+    "title": "Oracles",
+}
+pseudo_tested = {
+    "tag": "pseudo-tested-code",
+    "title": "Pseudo-Tested Code",
+}
+regression_testing = {
+    "tag": "regression-testing",
+    "title": "Regression Testing",
+}
+search_based_software_engineering = {
+    "tag": "search-based-software-engineering",
+    "title": "Search-Based Software Engineering",
+}
+search_based_test_generation = {
+    "tag": "search-based-test-generation",
+    "title": "Search-Based Test Generation",
+}
+software_maintenance = {
+    "tag": "software-maintenance",
+    "title": "Software Maintenance",
+}
+testability = {
+    "tag": "testability",
+    "title": "Testability",
+}
+testability_transformation = {
+    "tag": "testability-transformation",
+    "title": "Testability Transformation",
+}
+test_case_diversity = {
+    "tag": "test-case-diversity",
+    "title": "Test Case Diversity",
+}
+test_suite_maintenance = {
+    "tag": "test-suite-maintenance",
+    "title": "Test Suite Maintenance",
+}
+web_presentation_failures = {
+    "tag": "web-presentation-failures",
+    "title": "Web Presentation Failures",
+}
